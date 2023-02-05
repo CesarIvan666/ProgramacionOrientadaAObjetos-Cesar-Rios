7ce787b82ae91fd4460f1f14b2d3d92546d441a5
CesarIvan666 <123044062+CesarIvan666@users.noreply.github.com>
2023-02-04 18:40:54 -0600

    reto1suma

--- src/edu/cesarivan666/reto1/process/Calculadora.java
+++ src/edu/cesarivan666/reto1/process/Calculadora.java
@@ -6,9 +6,39 @@ public class Calculadora {
      * suma enteros
      *
      * @param operando1 primer operando para sumar
-     * @param operando1 primer operando para sumar
+     * @param operando2 primer operando para sumar
      **/
     public static int sumarNumeros(int operando1, int operando2){
         return operando1+operando2;
+    }
+    /**
+     *
+     * resta enteros
+     *
+     * @param operando1 primer operando para restar
+     * @param operando2 primer operando para restar
+     **/
+    public static int restarNumeros(int operando1, int operando2){
+        return operando1-operando2;
+    }
+    /**
+     *
+     * multiplicar enteros
+     *
+     * @param operando1 primer operando para multiplicar
+     * @param operando2 primer operando para multiplicar
+     **/
+    public static int multiplicarNumeros(int operando1, int operando2){
+        return operando1*operando2;
+    }
+    /**
+     *
+     * resta dividir
+     *
+     * @param operando1 primer operando dividir
+     * @param operando2 primer operando para dividir
+     **/
+    public static int dividirNumeros(int operando1, int operando2){
+        return operando1/operando2;
     }
 }
--- src/edu/cesarivan666/reto1/ui/CLI.java
+++ src/edu/cesarivan666/reto1/ui/CLI.java
@@ -5,19 +5,20 @@ import javax.swing.*;
 import java.util.Scanner;
 public class CLI {
     public static void MostrarMenu(){
-        System.out.println("Selecciona una opcion");
-        System.out.println("suma");
-        System.out.println("resta");
-        System.out.println("multiplicacion");
+        System.out.println("Selecciona una de las siguientes opciones en contenido numerico");
+        System.out.println("1 es suma");
+        System.out.println("2 es resta");
+        System.out.println("3 es multiplicacion");
+        System.out.println("4 es Division");
 
         Scanner scanner = new Scanner (System.in);
 
         int option = scanner.nextInt();
         scanner.nextLine();
 
-        System.out.println("introduce el primer valor");
+        System.out.println("introduce el primer valor en tipo entero");
         int operando1 = scanner.nextInt();scanner.nextLine();
-        System.out.println("introduce el segundo valor");
+        System.out.println("introduce el segundo valor en tipo entero");
         int operando2 = scanner.nextInt();scanner.nextLine();
         int resultado=-1;
         String operacion ="";
@@ -25,9 +26,21 @@ public class CLI {
         switch (option){
             case 1:
                 resultado = Calculadora.sumarNumeros(operando1, operando2);
+                System.out.printf("El resultado de la operacion de %d+%d = %d", operando1,operando2, resultado);
+                break;
+            case 2:
+                resultado = Calculadora.restarNumeros(operando1, operando2);
+                System.out.printf("El resultado de la operacion de %d-%d = %d", operando1,operando2, resultado);
+                break;
+            case 3:
+                resultado = Calculadora.multiplicarNumeros(operando1, operando2);
+                System.out.printf("El resultado de la operacion de %d*%d = %d", operando1,operando2, resultado);
+                break;
+            case 4:
+                resultado = Calculadora.dividirNumeros(operando1, operando2);
+                System.out.printf("El resultado de la operacion de %d/%d = %d", operando1,operando2, resultado);
                 break;
         }
-        System.out.printf("El resultado de la operacion de %d +%d = %d", operando1,operando2, resultado);
     }
 }
 
