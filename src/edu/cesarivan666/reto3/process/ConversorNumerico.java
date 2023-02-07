d1b7d29c78a819df0e6d61630918d445a07e2c55
CesarIvan666 <123044062+CesarIvan666@users.noreply.github.com>
2023-02-07 09:38:38 -0600

    Conversor de binario

---
+++ src/edu/cesarivan666/reto3/process/ConversorNumerico.java
@@ -0,0 +1,21 @@
+package edu.cesarivan666.reto3.process;
+
+/**
+ * Esta clase contiene metodos para convertir numeros de una base numerica a otra
+ */
+public class ConversorNumerico {
+    /**
+     * Este metodo recibe un string que representa un numero base decimal y regresa
+     * el mismo numero en su representacion de base binaria
+     *
+     * Qparam decimal numero a convertir
+     * @return numero a binario
+     */
+    public static String convertirDecimalBinario(String decimal){
+        return "10101010";
+    }
+
+
+
+
+}
