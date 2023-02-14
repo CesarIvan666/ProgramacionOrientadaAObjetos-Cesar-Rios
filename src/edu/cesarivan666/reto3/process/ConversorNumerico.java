848db8e0962c587361994d5c41b2abc827f143c0
CesarIvan666 <123044062+CesarIvan666@users.noreply.github.com>
2023-02-13 21:16:04 -0600

    reto3
    reto3

--- src/edu/cesarivan666/reto3/process/ConversorNumerico.java
+++ src/edu/cesarivan666/reto3/process/ConversorNumerico.java
@@ -12,36 +12,73 @@ package edu.cesarivan666.reto3.process;
 
 public class ConversorNumerico {
 
-    public static String decimalToBinary(String decimal) {
-        int decimalNumber = Integer.parseInt(decimal);
-        String binary = Integer.toBinaryString(decimalNumber);
-        return binary;
+    public static String decimalToBinary(int decimal) {
+        String binario = "";
+        while (decimal > 0) {
+            int resto = decimal % 2;
+            binario = resto + binario;
+            decimal = decimal / 2;
+        }
+        return binario;
     }
 
-    public static String decimalToOctal(String decimal) {
-        int decimalNumber = Integer.parseInt(decimal);
-        String octal = Integer.toOctalString(decimalNumber);
+    public static String decimalToOctal(int decimal) {
+        String octal = "";
+        while (decimal > 0) {
+            int resto = decimal % 8;
+            octal = resto + octal;
+            decimal = decimal / 8;
+        }
         return octal;
     }
 
-    public static String decimalToHexadecimal(String decimal) {
-        int decimalNumber = Integer.parseInt(decimal);
-        String hexadecimal = Integer.toHexString(decimalNumber);
+    public static String decimalToHexadecimal(int decimal) {
+        String hexadecimal = "";
+        while (decimal > 0) {
+            int resto = decimal % 16;
+            if (resto < 10) {
+                hexadecimal = resto + hexadecimal;
+            } else {
+                char c = (char) ('A' + (resto - 10));
+                hexadecimal = c + hexadecimal;
+            }
+            decimal = decimal / 16;
+        }
         return hexadecimal;
     }
-
-    public static int binaryToDecimal(String binary) {
-        int decimal = Integer.parseInt(binary, 2);
+    public static int binaryToDecimal(String binario) {
+        int decimal = 0;
+        int longitud = binario.length();
+        for (int i = 0; i < longitud; i++) {
+            int digito = Character.getNumericValue(binario.charAt(i));
+            decimal += digito * Math.pow(2, longitud - i - 1);
+        }
         return decimal;
     }
 
     public static int octalToDecimal(String octal) {
-        int decimal = Integer.parseInt(octal, 8);
+        int decimal = 0;
+        int longitud = octal.length();
+        for (int i = 0; i < longitud; i++) {
+            int digito = Character.getNumericValue(octal.charAt(i));
+            decimal += digito * Math.pow(8, longitud - i - 1);
+        }
         return decimal;
     }
-
     public static int hexadecimalToDecimal(String hexadecimal) {
-        int decimal = Integer.parseInt(hexadecimal, 16);
+        int decimal = 0;
+        int longitud = hexadecimal.length();
+        for (int i = 0; i < longitud; i++) {
+            char c = hexadecimal.charAt(i);
+            int digito;
+            if (c >= '0' && c <= '9') {
+                digito = Character.getNumericValue(c);
+            } else {
+                digito = 10 + (c - 'A');
+            }
+            decimal += digito * Math.pow(16, longitud - i - 1);
+        }
         return decimal;
     }
 }
+
