34f43166910663042be4f0a309d4bad6554ed890
CesarIvan666 <123044062+CesarIvan666@users.noreply.github.com>
2023-02-18 17:05:25 -0600

    reto4

--- src/edu/cesarivan666/reto4/Main.java
+++ src/edu/cesarivan666/reto4/Main.java
@@ -3,13 +3,14 @@ package edu.cesarivan666.reto4;
 import edu.cesarivan666.reto4.ui.CLI;
 
 /**
- *Ejecución del programa
+ * Clase que contiene el método main para ejecutar la aplicación.
  */
 public class Main {
-
-        public static void main(String[] args){
-            CLI.showmenu();
-        }
+    /**
+     * Método principal que ejecuta el método main de la clase CLI.
+     * @param args Argumentos de línea de comandos (no se utilizan en esta aplicación).
+     */
+    public static void main(String[] args){
+        CLI.main();
     }
-
-
+}
