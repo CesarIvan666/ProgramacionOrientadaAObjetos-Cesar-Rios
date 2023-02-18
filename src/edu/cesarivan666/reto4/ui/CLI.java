0e06d9e76fe1ec5d8e3175818339eb1894226fab
CesarIvan666 <123044062+CesarIvan666@users.noreply.github.com>
2023-02-18 17:05:25 -0600

    reto4

--- src/edu/cesarivan666/reto4/ui/CLI.java
+++ src/edu/cesarivan666/reto4/ui/CLI.java
@@ -1,52 +1,57 @@
 package edu.cesarivan666.reto4.ui;
 import edu.cesarivan666.reto4.process.AnalisisVentas;
+/**
 
+ Esta clase representa una interfaz de línea de comando para el análisis de ventas.
+ */
 public class CLI {
+    /**
 
-    public static void showmenu() {
-        /**
-         * Creamos un arreglo bidimensional para almacenar los datos
-         */
-        int[][] ventas = {
-                {111, 192, 289, 415},
-                {483, 500, 470, 114},
-                {471, 355, 474, 161},
-                {427, 158, 160, 308}
-        };
-        /**
-         * Trimestre que más se vende por cada sabor de café frío
-         */
-        int[] maxVentasPorSabor = new int[4];
-        String[] sabores = {"chocolate", "vainilla", "fresa", "oreo"};
-        for (int i = 0; i < sabores.length; i++) {
-            int maxVentas = 0;
-            for (int j = 0; j < 4; j++) {
-                if (ventas[j][i] > maxVentas) {
-                    maxVentas = ventas[j][i];
-                    maxVentasPorSabor[i] = j;
-                }
-            }
-            System.out.println("El trimestre que más se vende de " + sabores[i] + " es: "
-                    + AnalisisVentas.obtenerTrimestre(maxVentasPorSabor[i]) + ".");
-        }
-        /**
-         * Trimestre que más producto vende en general
-         */
-        int maxVentasTotal = 0;
-        int trimestreMaxVentasTotal = 0;
-        for (int i = 0; i < 4; i++) {
-            int totalVentas = 0;
-            for (int j = 0; j < 4; j++) {
-                totalVentas += ventas[i][j];
-            }
-            if (totalVentas > maxVentasTotal) {
-                maxVentasTotal = totalVentas;
-                trimestreMaxVentasTotal = i;
-            }
-        }
-        System.out.println("El trimestre que más producto vende en general es: "
-                + AnalisisVentas.obtenerTrimestre(trimestreMaxVentasTotal) + ".");
+     Imprime el trimestre que más se vende de cada sabor en un arreglo de máximo de ventas por sabor y un arreglo de nombres de sabores.
+     @param maxVentasPorSabor un arreglo de máximo de ventas por sabor
+     @param sabores un arreglo de nombres de sabores
+     */
+        public static void imprimirMaxVentasPorSabor(int[] maxVentasPorSabor, String[] sabores) {
+         for (int i = 0; i < sabores.length; i++) {
+             System.out.println("El trimestre que más se vende de " + sabores[i] + " es: " + obtenerTrimestre(maxVentasPorSabor[i]) + ".");
+         }
+     }
+    /**
+     Imprime el trimestre que más vende en general en una matriz de ventas.
+     @param trimestreMaxVentasTotal el índice del trimestre que más vende en general
+     */
+     public static void imprimirTrimestreMaxVentasTotal(int trimestreMaxVentasTotal) {
+         System.out.println("El trimestre que más producto vende en general es: " + obtenerTrimestre(trimestreMaxVentasTotal) + ".");
+     }
+    /**
+
+     Convierte el índice de un trimestre a su nombre en formato "MMM-YYY".
+     @param trimestre el índice del trimestre (0 para ene-feb-mar, 1 para abr-may-jun, etc.)
+     @return el nombre del trimestre en formato "MMM-YYY" (por ejemplo, "ene-feb-mar")
+     */
+     public static String obtenerTrimestre(int trimestre) {
+         switch (trimestre) {
+             case 0:
+                 return "ene-feb-mar";
+             case 1:
+                 return "abr-may-jun";
+             case 2:
+                 return "jul-agos-sep";
+             case 3:
+                 return "oct-nov-dic";
+             default:
+                 return "trimestre no válido";
+         }
+     }
+    /**
+
+     Función principal que llama a la función principal de la clase AnalisisVentas.
+     */
+    public static void main() {
+        AnalisisVentas.main();
     }
-
-
 }
+
+
+
+
